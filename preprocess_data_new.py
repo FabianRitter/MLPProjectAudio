a2fd42108161f3b8edbefdb859cc9acc680c6017
FabianRitter <fabian.acustica@gmail.com>
2019-02-10 12:11:40 +0000

    adding bash file to run experiments and preprocessed.py improved to use optional arguments

--- preprocess_data_new.py
+++ preprocess_data_new.py
@@ -1,29 +1,79 @@
 import numpy as np
+import librosa
 from librosa.feature import melspectrogram
 import pandas as pd
 import soundfile
+import argparse
+import yaml
 import os
+import h5py
 
 
+
+
+####### ARGUMENTS (JUST THE PATH FOR NOW) #######
+
+parser = argparse.ArgumentParser(description='Code for DCASE Challenge task 2.')
+
+parser.add_argument('-p','--params',dest='params_preprocessing',action='store',
+                        required=False,type=str)
+
+parser.add_argument('-e','--expnum',dest='experiment_number', action='store',
+                        required=False,type=int)
+
+
+args = parser.parse_args()
+
+
+if args.params_preprocessing:
+    params = yaml.load(open(args.params_preprocessing))
+    params_ctrl = params['ctrl']
+    path_to_metadata = params_ctrl.get('dataset_path')
+    type_training = params_ctrl.get('type_training')
+    path_to_metadata = '../../real_data/FSDnoisy18k.meta/' + type_training + ".csv"
+    base_path = '../../real_data/FSDnoisy18k.audio_' + type_training
+    print('base path is', base_path)
+else:
 #CHANGE PATHS
-path_to_metadata = '../../real_data/FSDnoisy18k.meta/train.csv'
-base_path = '../../real_data/FSDnoisy18k.audio_train'
+    path_to_metadata = '../../real_data/FSDnoisy18k.meta/train.csv'
+    base_path = '../../real_data/FSDnoisy18k.audio_train'
+
+if args.experiment_number:
+    experiment_number= args.experiment_number
+else:
+    experiment_number = False
+
+
 
 df_train = pd.read_csv(path_to_metadata)
 fname = df_train['fname'].values
 
-number_of_frames = 88200#88200 # they use 88200 
-n_mels = 96
-fmax = 22050
+if experiment_number and type_training == "train":
+    if experiment_number == 176:
+        fname = fname[100 * (experiment_number-1):]
+    fname = fname[100 * (experiment_number-1): 100 * experiment_number]
+    print("the length of fname is", len(fname))
+else:
+	if experiment_number == 10:
+		fname = fname[100 * (experiment_number-1):]
+	fname = fname[100 * (experiment_number-1): 100 * experiment_number]
+	print('using {0} files for testing data'.format(len(fname)))
+
+
+
+
+n_mels = 64
+win_length_samples = 512
+fs= 16000 # we will make downsampling to save some data!!44100
+number_of_frames = fs * 2 # two seconds of data-88200#88200 # they use 88200 
+hop_length_samples = int(win_length_samples / 2)
+fmax = int(fs / 2)
 fmin = 0
-fs= 44100
-hop_length_samples = 882
-n_fft = 2048
+n_fft = 1024
 normalize_audio = True
 patch_hop = 50
 patch_len = 100
 spectrogram_type = 'power'
-win_length_samples = 1764
 
 
 def normalize_mel_histogram(mel_hist, number_of_frames=100):
@@ -52,7 +102,7 @@ def normalize_mel_histogram(mel_hist, number_of_frames=100):
     else:
         return mel_hist
     
-def convert2mel(audio,base_path):
+def convert2mel(audio,base_path,fs,fmax,n_mels,number_of_frames):
     """
     Convert raw audio to mel spectrogram
     """
@@ -61,8 +111,8 @@ def convert2mel(audio,base_path):
     data = data.T
     # Resample if the source_fs is different from expected
     if fs != source_fs:
-        data = librosa.core.resample(data, source_fs, params_extract.get('fs'))
-        print('Resampling to %d: %s' % (params_extract.get('fs'), file_path))
+        data = librosa.core.resample(data, source_fs,fs)
+        print('Resampling to %d: %s' % (fs, base_path) )
     ### extracted from Eduardo Fonseca Code, it seems there are 3 audio corrupted so we need to check length
     
     if len(data) > 0 :
@@ -71,18 +121,15 @@ def convert2mel(audio,base_path):
         ###### tenemos que ver como borrar estos audios! 
         data = np.ones((number_of_frames, 1))
         print('File corrupted. Could not open: %s' % path)
+    
     #data = np.reshape(data, [-1, 1])        
-    #data = data.astype(float)
     
     ## shor time processing of audio #####
+
     
-    
-    
-    
-    
-    mels = melspectrogram(y=data, sr=44100,
+    mels = melspectrogram(y=data, sr=fs,
                             n_fft=2048, hop_length=hop_length_samples,
-                            power=2.0, n_mels=n_mels,fmax=fmax) 
+                            power=2, n_mels=n_mels,fmax=fmax) 
     mel_normalized = normalize_mel_histogram(mels.T,number_of_frames)
     mel_norm_flat = mel_normalized.flatten()
     return mel_norm_flat
@@ -98,25 +145,29 @@ def normalize_amplitude(y, tolerance=0.005):
     max_value = max(abs(y)) + tolerance
     return y / max_value
 
-
-fname2= fname[:100]
-
 processes = []
 
-print(len(fname2))
 
-all_inputs = np.zeros([len(fname2),n_mels*number_of_frames])
-#all_inputs= []
-all_targets = np.zeros(len(fname2))
-#all_manually = np.zeros(len(fname))
-#all_noisy_small = np.zeros(len(fname))
+all_inputs = np.zeros([len(fname),n_mels*number_of_frames])
+
+all_targets = np.zeros(len(fname))
+
+if type_training == 'train':
+    all_manually = np.zeros(len(fname))
+    all_noisy_small = np.zeros(len(fname))
+
 all_dict = {}
-all_inputs = [convert2mel(audio,base_path) for audio in fname2]                      
-#all_inputs.append([convert2mel(audio,base_path) for audio in fname])
-#np.asarray(all_inputs, dtype=np.float32)
+all_inputs = [convert2mel(audio,base_path,fs,fmax,n_mels,number_of_frames) for audio in fname]                      
 all_dict['inputs'] = all_inputs
 all_dict['targets'] =  df_train['label']
-#all_dict['manually_verified'] = df_train['manually_verified']
-#all_dict['noisy_small'] = df_train['noisy_small']
 
-np.savez('processed_data-train.npz',**all_dict)
+if type_training == 'train':
+    all_dict['manually_verified'] = df_train['manually_verified']
+    all_dict['noisy_small'] = df_train['noisy_small']
+
+if experiment_number:
+    print("saving data for experiment" , experiment_number)
+    path_hdd = "/media/fabian/Seagate Expansion Drive/MLP_EXPERIMENTS/"
+    np.savez(path_hdd + 'TestData/processed_data-test-{' + str(experiment_number) + "}.npz",**all_dict)
+
+
