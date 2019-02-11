27c85692ad692cb1fe97e157d2bbff6a33cadea9
Jordi <jordi299@gmail.com>
2019-02-11 10:46:45 +0000

    added path

--- compress.py
+++ compress.py
@@ -1,6 +1,6 @@
 import os
 import zipfile
-path_to_zip_file = 'FSDnoisy18k.meta.zip'
+path_to_zip_file = '../raw_audio.zip'
 directory_to_extract_to = 'audio'
 zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
 zip_ref.extractall(directory_to_extract_to)
